218debf182cd483c41880dd6a04ba845f448096b
4leksey8963 <gradix8953@gmail.com>
2026-02-03 00:24:52 +0300

    qweqwe

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/simpliflow',
+  base: '/simpliflow/',
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
